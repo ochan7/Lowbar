5131646ada80a0433ff72c55fafa3e1d43607ebb
olie.chan.141@gmail.com <olie.chan.141@gmail.com>
2017-10-07 10:15:44 +0100

    passed tests for identity function

--- spec/core_lowbar.spec.js
+++ spec/core_lowbar.spec.js
@@ -8,3 +8,22 @@ describe('_', () => {
         expect(_).to.be.an('object');
     });
 });
+
+describe.only('#identity', () => {
+    it('it is a function', () => {
+        expect(_.identity).to.be.a('function');
+    });
+    it('returns undefined when given no arguments', () => {
+        expect(_.identity()).to.equal(undefined);
+    });
+    it('returns the argument given to it', () => {
+        expect(_.identity('hello')).to.equal('hello');
+        expect(_.identity(5)).to.equal(5);
+        expect(_.identity(undefined)).to.equal(undefined);
+    });
+    it('returns the same object or array given to it', () => {
+        const arr = ['hello'], obj = {1:'hello'};
+        expect(_.identity(arr)).to.equal(arr);
+        expect(_.identity(obj)).to.equal(obj);
+    });
+});
