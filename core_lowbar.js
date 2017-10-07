7eeffd5e1a6d4ff5c21d4649e565f21845b03392
olie.chan.141@gmail.com <olie.chan.141@gmail.com>
2017-10-07 10:59:46 +0100

    passed tests for values function

--- core_lowbar.js
+++ core_lowbar.js
@@ -1,7 +1,17 @@
 const _ = {};
-
-_.identity = function(x) {
+_.identity = (x) => {
     return x;
 };
 
+_.values = (obj) => {
+    if (typeof obj !== 'object' || obj === null) return [];
+    else {
+        const values = [];
+        if (Array.isArray(obj)) return obj;
+        for (let key in obj) {
+            values.push(obj[key]);
+        }
+        return values;
+    }
+};
 module.exports = _;
--- spec/core_lowbar.spec.js
+++ spec/core_lowbar.spec.js
@@ -9,7 +9,7 @@ describe('_', () => {
     });
 });
 
-describe.only('#identity', () => {
+describe('#identity', () => {
     it('it is a function', () => {
         expect(_.identity).to.be.a('function');
     });
@@ -25,5 +25,24 @@ describe.only('#identity', () => {
         const arr = ['hello'], obj = {1:'hello'};
         expect(_.identity(arr)).to.equal(arr);
         expect(_.identity(obj)).to.equal(obj);
+    });
+});
+
+describe.only('#values', () => {
+    it('it is a function', () => {
+        expect(_.values).to.be.a('function');
+    });
+    it('returns an empty object when not given an array or object', () => {
+        expect(_.values()).to.eql([]);
+        expect(_.values('hello')).to.eql([]);
+        expect(_.values(null)).to.eql([]);
+    });
+    it('returns the array when given an array', () => {
+        expect(_.values(['hello'])).to.eql(['hello']);
+        expect(_.values([1,2,3])).to.eql([1,2,3]);
+    });
+    it('returns an array of values when given an object', () => {
+        expect(_.values({0:0, 1:1})).to.eql([0,1]);
+        expect(_.values({one: 1, two: 2, three: 3})).to.eql([1,2,3]);
     });
 });
