34f2071dc5209e19b2d1673b49d917799669ef91
olie.chan.141@gmail.com <olie.chan.141@gmail.com>
2017-10-18 19:34:35 +0100

    shuffle passes tests

--- advanced_lowbar.js
+++ advanced_lowbar.js
@@ -11,4 +11,19 @@ _.once = (func) => {
     };
 };
 
+_.shuffle = (list) => {
+ let newList = [];
+ if (typeof list === 'string') newList = list.split('');
+ if (typeof list === 'object') newList = _.values(list);
+ newList = newList.slice();
+ let n = newList.length, index, placeholder;
+    while (n) {
+        index = Math.floor(Math.random() * n--);
+        placeholder = newList[n];
+        newList[n] = newList[index];
+        newList[index] = placeholder;
+    }
+ return newList;
+};
+
 module.exports = _;
--- spec/advanced_lowbar.spec.js
+++ spec/advanced_lowbar.spec.js
@@ -17,3 +17,30 @@ describe('#once', () => {
         expect(spy.callCount).to.equal(1);
     });
 });
+
+describe.only('#shuffle', () => {
+    it('it is a function', () => {
+        expect(_.shuffle).to.be.a('function');
+    });
+    it('it returns an empty array when not given a valid list', () => {
+        expect(_.shuffle()).to.eql([]);
+        expect(_.shuffle(5656474)).to.eql([]);
+        expect(_.shuffle([])).to.eql([]);
+        expect(_.shuffle({})).to.eql([]);
+    });
+    it('it returns a list of the same length', () => {
+        expect(_.shuffle([1,2,3,4]).length).to.equal(4);
+    });
+    it('it does not mutate the array given to it', () => {
+        let arr = [1,2,3,4,5];
+        expect(_.shuffle(arr)).to.not.equal(arr);
+    });
+    it('it returns a shuffled copy of the list', () => {
+        const arr = [1,2,3,4,5,5,6];
+        const str = 'hello';
+        const obj = {0:0, 1:1, 2:2, 3:3};
+        expect(_.shuffle(arr)).to.not.eql(arr);
+        expect(_.shuffle(str)).to.not.eql(str);
+        expect(_.shuffle(obj)).to.not.eql(obj);
+    });
+});
